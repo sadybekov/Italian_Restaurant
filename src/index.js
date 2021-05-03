541657e3b24fc7b06635d20eb7f9ae6f77085a62
Ilyas <71405596+sadybekov@users.noreply.github.com>
2021-05-02 23:05:27 -0600

    rendering

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const startersItems = [];
 const pastaItems = [];
 const pizzaItems = [];
 
+//Sorting
+
 const menuSorting = (menu) => {
   for (let i = 0; i < menu.length; i++) {
     if (menu[i].type === "pasta") {
@@ -24,7 +26,46 @@ const menuSorting = (menu) => {
   return sortedMenu;
 };
 
-menuSorting(menuItems);
+//Rendering
 
-console.log("sorted");
-console.log(sortedMenu);
+const renderSection = (id) => {
+  const section = document.getElementById(id);
+  let menu = null;
+  if (id === "starters") {
+    menu = sortedMenu[0];
+  } else if (id === "pasta") {
+    menu = sortedMenu[1];
+  } else {
+    menu = sortedMenu[2];
+  }
+
+  for (let i = 0; i < menu.length; i++) {
+    const elementName = document.createElement("h3");
+    const contentName = document.createTextNode(`${menu[i].name} `);
+    const contentPrice = document.createTextNode(` \\$${menu[i].price}`);
+
+    elementName.appendChild(contentName);
+    elementName.appendChild(contentPrice);
+    if (menu[i].spicy === true) {
+      elementName.classList.add("spicy");
+    }
+
+    section.appendChild(elementName);
+
+    const elementDescription = document.createElement("h5");
+    const contentDescription = document.createTextNode(
+      `${menu[i].description}`
+    );
+    elementDescription.appendChild(contentDescription);
+    section.appendChild(elementDescription);
+  }
+};
+
+const renderMenu = () => {
+  renderSection("starters");
+  renderSection("pasta");
+  renderSection("pizza");
+};
+
+menuSorting(menuItems);
+renderMenu();
